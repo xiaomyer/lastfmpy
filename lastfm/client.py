58e441dd5a040552af2b9e38b30c4cc6b5b6bd29
MyerFire <myercheng0614@gmail.com>
2021-01-27 14:50:20 -0500

    finished artists methods

--- lastfm/client.py
+++ lastfm/client.py
@@ -37,14 +37,27 @@ class Artist:
     def __init__(self, api):
         self.api = api
 
-    async def get_correction(self, artist: str) -> objects.Artist:
-        json = await requests.get(self.api, "artist.getcorrection", artist=artist)
-        return objects.Artist(json["artist"])
-
     async def get_info(self, artist: str, *, autocorrect: bool = False, username: str = None) -> objects.Artist:
         json = await requests.get(self.api, "artist.getinfo", artist=artist, autocorrect=autocorrect,
                                   username=username)
         return objects.Artist(json["artist"])
+
+    async def get_correction(self, artist: str) -> objects.Artist:
+        json = await requests.get(self.api, "artist.getcorrection", artist=artist)
+        return objects.Artist(json["artist"])
+
+    async def get_similar(self, artist: str, *, autocorrect: bool = False, limit: int = 0):
+        json = await requests.get(self.api, "artist.getsimilar", artist=artist, limit=limit, autocorrect=autocorrect)
+        return [objects.Track(track) for track in json["similarartists"]["artist"]]
+
+    async def get_top_albums(self, artist: str, *, autocorrect: bool = False, limit: int = 0, page: int = 0):
+        json = await requests.get(self.api, "artist.gettopalbums", artist=artist, limit=limit, page=page,
+                                  autocorrect=autocorrect)
+        return [objects.Track(track) for track in json["topalbums"]["album"]]
+
+    async def get_top_tags(self, artist: str, *, autocorrect: bool = False):
+        json = await requests.get(self.api, "artist.gettoptags", artist=artist, autocorrect=autocorrect)
+        return [objects.Tag(track) for track in json["toptags"]["tag"]]
 
     async def get_top_tracks(self, artist: str, *, autocorrect: bool = False, limit: int = 0, page: int = 0):
         json = await requests.get(self.api, "artist.gettoptracks", artist=artist, limit=limit, page=page,
